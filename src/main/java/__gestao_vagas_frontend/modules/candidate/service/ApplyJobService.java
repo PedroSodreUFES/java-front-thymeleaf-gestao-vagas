ed9bce8061761010b09badeab6d4a1466e8b7a5c
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-11-23 01:32:00 -0300

    feat: aplicar para vaga + listar vagas

--- src/main/java/__gestao_vagas_frontend/modules/candidate/service/ApplyJobService.java
+++ src/main/java/__gestao_vagas_frontend/modules/candidate/service/ApplyJobService.java
@@ -7,8 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
